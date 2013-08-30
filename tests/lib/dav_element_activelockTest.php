a1d3c5375856cf9e7a41ebc9e3f10e93afb3006a
Niek Bosch <niek@niekbosch.nl>
2013-08-30 12:17:51 +0200

    Added tests for DAV_Element_activelock class

---
+++ tests/lib/dav_element_activelockTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Contains tests for the DAV_Element_activelock class
+ * 
+ * Copyright ©2013 SURFsara b.v., Amsterdam, The Netherlands
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at <http://www.apache.org/licenses/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package DAV
+ * @subpackage tests
+ */
+
+/**
+ * Contains tests for the DAV_Element_activelock class
+ * @package DAV
+ * @subpackage tests
+ */
+class DAV_Element_activelockTest extends PHPUnit_Framework_TestCase {
+  
+  private $lock = null;
+
+  
+  protected function setUp() {
+    $this->timeout = time() + 3600;
+    $this->lock = new DAV_Element_activelock( array(
+        'lockroot' => '/path',
+        'depth' => DAV::DEPTH_INF,
+        'locktoken' => 'qwerty',
+        'owner' => '/path/to/user',
+        'timeout' => $this->timeout
+    ) );
+    DAV::$SUBMITTEDTOKENS[ 'qwerty' ] = '';
+  }
+
+  public function testConstructor() {
+    $this->assertEquals( '/path'        , $this->lock->lockroot , 'DAV_Element_activelock::__constructor should set lockroot attribute' );
+    $this->assertEquals( DAV::DEPTH_INF , $this->lock->depth    , 'DAV_Element_activelock::__constructor should set depth attribute' );
+    $this->assertEquals( 'qwerty'       , $this->lock->locktoken, 'DAV_Element_activelock::__constructor should set locktoken attribute' );
+    $this->assertEquals( '/path/to/user', $this->lock->owner    , 'DAV_Element_activelock::__constructor should set owner attribute' );
+    $this->assertEquals( $this->timeout , $this->lock->timeout  , 'DAV_Element_activelock::__constructor should set timeout attribute' );
+  }
+
+
+  public function testToXML() {
+    $this->lock->timeout = time() - 1; // Pretend the lock has timed out, because else we can't be sure what the timeout value in the XML will be
+    $this->assertEquals( "<D:activelock>\n<D:lockscope><D:exclusive/></D:lockscope>\n<D:locktype><D:write/></D:locktype>\n<D:depth>infinity</D:depth>\n<D:owner>/path/to/user</D:owner>\n<D:timeout>Second-0</D:timeout>\n<D:locktoken>\n<D:href>qwerty</D:href>\n</D:locktoken>\n<D:lockroot><D:href>/path</D:href></D:lockroot>\n</D:activelock>", $this->lock->toXML(), 'DAV_Element_activelock::toXML() should return the correct XML string' );
+  }
+
+
+  public function testToJSON() {
+    $this->assertEquals( json_encode( $this->lock ), $this->lock->toJSON(), 'DAV_Element_activelock::toJSON() should return the correct json value' );
+  }
+
+
+  public function testFromJSON() {
+    $json = $this->lock->toJSON();
+    $this->assertEquals( $this->lock, DAV_Element_activelock::fromJSON( $json ), 'DAV_Element_activelock::fromJSON() should create a propper new object' );
+    $this->lock->timeout = time() - 1; // Pretend the lock has not timed out
+    $timedout_json = $this->lock->toJSON();
+    $this->assertNull( DAV_Element_activelock::fromJSON( $timedout_json ), 'DAV_Element_activelock::fromJSON() should return null when the lock has timed out' );
+  }
+
+} // class DAV_Element_lockdiscoveryTest
+
+// End of file
