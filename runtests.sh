df2feddeedd93948a77586971ef50b691be10c8d
Niek Bosch <niek.bosch@surfsara.nl>
2013-09-18 12:10:59 +0200

    Updated runtests.sh so it uses phpunit as installed by composer

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-phpunit --bootstrap ${DIR}/tests/bootstrap.php ${DIR}/tests
+./vendor/bin/phpunit --bootstrap ${DIR}/tests/bootstrap.php ${DIR}/tests
